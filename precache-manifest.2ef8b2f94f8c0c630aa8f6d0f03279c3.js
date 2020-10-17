84b1fcb38d2bf5e846a637043d48d6af6b37163c
Yazheviks <vicsergeyeva@gmail.com>
2020-10-17 17:33:20 +0300

    Updates

---
+++ precache-manifest.2ef8b2f94f8c0c630aa8f6d0f03279c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c59c78b02921e716cac5e9a44085fafb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "492049b48916edc8bd00",
+    "url": "./static/css/main.e23459e2.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "492049b48916edc8bd00",
+    "url": "./static/js/main.eef76f75.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
